5823f7c23226e4c5542f96c5a662b956121b3432
Paul Campbell <paul.campbell@stonybrook.edu>
2014-10-20 20:31:00 -0400

    Utility.h
    Added extra functions for creating a server socket.

--- include/utility.h
+++ include/utility.h
@@ -1,12 +1,19 @@
 #ifndef UTILITY_H
 #define UTILITY_H
+// stdlib headers
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+// system headers
+#include <sys/socket.h>
+#include <arpa/inet.h>
+// project headers
 #include "debug.h"
-
+// Utility constants
 #define BUFFER_SIZE 256
-
+#define SERVER_SOCKET_BIND_FAIL -1
+// Utility structs
 typedef struct {
 	unsigned int port;
 	unsigned int mtu;
@@ -21,5 +28,13 @@ typedef struct {
  * else false.
  */
 bool parseConfig(char *path, Config *config);
+
+/**
+ * Binds a UDP socket to the provided port.
+ * @param port Port number to bind server socket to.
+ * @return Returns the fd for the socket if creation was a success, 
+ * else SERVER_SOCKET_BIND_FAIL is returned.
+ */
+int createServer(unsigned int port);
 
 #endif
