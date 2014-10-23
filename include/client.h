72f12158f3ddb0c4f793136bf115404c740fe841
Shane Harvey <shane6harvey@yahoo.com>
2014-10-22 20:01:16 -0400

    Client
    Added functions to choose the IP's and create the socket.

--- include/client.h
+++ include/client.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 // system headers
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -13,7 +14,24 @@
 #include "utility.h"
 #include "debug.h"
 
-int handshake(Config *config);
+/**
+ * Determine the IP to identify the server and the IP to identify the client.
+ * If the address is on the same host then both IP's are set to 127.0.0.1
+ * Otherwise server_ip is set to the config value and we check if one of our
+ * interfaces is local. client_ip is set to the longest prefix matching IP.
+ * If non are local we choose and arbitrary address for the client.
+ *
+ * @param config     The config from client.in
+ * @param server_ip  The IP of the server
+ * @param client_ip  The IP of the client
+ * @return Returns true if the address is local
+ */
+bool chooseIPs(Config *config, struct in_addr *server_ip, struct in_addr *client_ip);
+
+
+int createSocket(struct sockaddr_in *serv_addr, struct sockaddr_in *client_addr, bool local);
+
+int handshake(Config *config, int sockfd);
 
 int run(int conn_fd, Config *config);
 
--- src/client/client.c
+++ src/client/client.c
@@ -6,6 +6,14 @@ int main(int argc, char *argv[]) {
 	int fd;
 	char *path = "client.in";
 	Config config;
+	struct sockaddr_in serv_addr, client_addr;
+	bool local;
+
+	/* Zero out the structs */
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	memset(&client_addr, 0, sizeof(client_addr));
+	client_addr.sin_family = AF_INET;
 
 	debug("Begin parsing config file: %s\n", path);
 	/* Attempt to parse the config */
@@ -17,8 +25,15 @@ int main(int argc, char *argv[]) {
 		debug("Seed: %u\n", config.seed);
 		debug("Prob loss: %f%%\n", config.loss);
 		debug("Mean: %u\n", config.mean);
+
+		local = chooseIPs(&config, &serv_addr.sin_addr, &client_addr.sin_addr);
+		/* finish initializing addresses */
+		serv_addr.sin_port = htons(config.port);
+		client_addr.sin_port = htons(0);
+
+		fd = createSocket(&serv_addr, &client_addr, local);
 		/* Config was successfully parsed; attempt to connect to server */
-		if((fd = handshake(&config)) >= 0) {
+		if((fd = handshake(&config, fd)) >= 0) {
 			/* Start the client producer/consumer threads */
 			run(fd, &config);
 		} else {
@@ -34,26 +49,58 @@ int main(int argc, char *argv[]) {
 	return EXIT_SUCCESS;
 }
 
-int handshake(Config *config) {
-	int fd;
-	struct sockaddr_in servaddr;    /* server address */
+bool chooseIPs(Config *config, struct in_addr *server_ip,
+				struct in_addr *client_ip) {
+	bool local = true;
+	/* TODO: call get_ifi_info_plus */
+
+	/* TODO: check if we match the server's address */
+
+	/* TODO: Else use the longest match */
+	/* For now just set server_ip to the config */
+	server_ip->s_addr = config->serv_addr.s_addr;
+	/* TODO: Else choose first IP from ifi_info */
+	/* for now just set client_ip to loopback address */
+	client_ip->s_addr = htonl(INADDR_LOOPBACK);
+
+	printf("Server address is local. IPserver: %s, IPclient %s\n",
+		inet_ntoa(*server_ip),inet_ntoa(*client_ip));
+	return local;
+}
+
+int createSocket(struct sockaddr_in *serv_addr, struct sockaddr_in *client_addr,
+				 bool local) {
+	int sockfd;
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(sockfd < 0) {
+		debug("Failed to create socket\n");
+		return sockfd;
+	}
+	/* bind socket to the client's address */
+	if(bind(sockfd, (struct sockaddr*)client_addr,
+			sizeof(struct sockaddr_in)) < 0) {
+		debug("Failed to bind to client address\n");
+		perror("createSocket: bind");
+		close(sockfd);
+		return -1;
+	}
+	/* connect the DG socket to the server address */
+	if(connect(sockfd, (struct sockaddr*)client_addr,
+				sizeof(struct sockaddr_in)) < 0) {
+		debug("Failed to connect to server address\n");
+		perror("createSocket: connect");
+		close(sockfd);
+		return -1;
+	}
+	return sockfd;
+}
+
+int handshake(Config *config, int sockfd) {
 	char *my_message = "this is a test message";
 
-	memset((char*)&servaddr, 0, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(config->port);
-
-	/* copy the servers' address into the sockaddr_in structure */
-	memcpy((void *)&servaddr.sin_addr, (void *)&config->serv_addr, sizeof(struct in_addr));
-
-	/* Socket */
-	if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		perror("cannot create socket");
-		return 0;
-	}
-
 	/* send a message to the server */
-	if (sendto(fd, my_message, strlen(my_message), 0, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+	if (send(sockfd, my_message, strlen(my_message), 0) < 0) {
 		perror("sendto failed");
 		return 0;
 	}
