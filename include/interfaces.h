6bdb602989e254c5ef1488ca9a2a120e8c1f9381
Paul Campbell <paul.campbell@stonybrook.edu>
2014-10-25 00:16:16 -0400

    Interfaces
    Changed the types of the socket fd in the interfaces struct. Added a
    function to free all allocated memory in the interfaces list.

--- include/interfaces.h
+++ include/interfaces.h
@@ -1,18 +1,20 @@
 #ifndef INTERFACES_H
 #define INTERFACES_H
 #define INTERFACES_BUFFER 256
+#include <stdlib.h>
+
 /**
  * Container used for holding information
  * about an interface.
  */
-typedef struct Interface {
-	unsigned int sockfd;
+typedef struct _Interface {
+	int sockfd;
 	char name[INTERFACES_BUFFER];
 	char ip_address[INTERFACES_BUFFER];
 	char network_mask[INTERFACES_BUFFER];
 	char subnet_address[INTERFACES_BUFFER];
-	struct Interface *next;
-	struct Interface *prev;
+	struct _Interface *next;
+	struct _Interface *prev;
 } Interface;
 
 /**
--- src/server/interfaces.c
+++ src/server/interfaces.c
@@ -1,7 +1,15 @@
 #include "interfaces.h"
 
-/*
-void destroy_interfaces(Interface **list) {
-
+static void _destroy_interfaces(Interface *node) {
+	if(node != NULL) {
+		_destroy_interfaces(node->next);
+	}
+	free(node);
 }
-*/
+
+void destroy_interfaces(Interface **list) {
+	_destroy_interfaces(*list);
+	if(*list != NULL) {
+		*list = NULL;
+	}
+}
