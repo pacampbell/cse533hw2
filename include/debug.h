7398e2eafbfb4f42ec07c42aa3be7080609c6fef
Paul Campbell <paul.campbell@stonybrook.edu>
2014-10-25 00:00:33 -0400

    Debug
    Added color constants to the debug file.

--- include/debug.h
+++ include/debug.h
@@ -2,8 +2,18 @@
 #define DEBUG_H
 #include <stdio.h>
 
+// Colors
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
 #ifdef DEBUG
-	#define debug(S, ...) do{fprintf(stderr, "DEBUG: %s:%d " S, __FILE__, __LINE__, ##__VA_ARGS__);} while(0)
+	#define debug(S, ...) do{fprintf(stderr, KBLU "DEBUG: %s:%d " S KNRM, __FILE__, __LINE__, ##__VA_ARGS__);} while(0)
 #else
 	#define debug(S, ...)
 #endif
