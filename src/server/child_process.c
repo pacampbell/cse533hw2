c483dd2d339a98cb54275ee0defb5495d76661f8
Paul Campbell <paul.campbell@stonybrook.edu>
2014-10-26 16:06:22 -0400

    Processes
    Added the function get_process_by_pid. Implemented get_process_by_pid,
    and remove_process.

--- src/server/child_process.c
+++ src/server/child_process.c
@@ -25,8 +25,44 @@ Process* add_process(Process **processes, Process *process) {
 }
 
 bool remove_process(Process **processes, Process *process) {
-	error("Fuction not implemented.\n");
-	return false;
+	bool removed = false;
+	Process *node = NULL;
+	// Make sure we have a valid process list
+	if(processes == NULL || *processes ==  NULL) {
+		error("Attempted to remove a process from a NULL list.\n");
+		goto finished;
+	}
+	// Make sure we have a valid process to search for
+	if(process == NULL) {
+		error("Attempted to remove a non-existant process from the process list.\n");
+	}
+	// We made it this far, lets search for the process and remove it.
+	node = *processes;
+	while(node != NULL) {
+		if(node->pid == process->pid) {
+			removed = true;
+			// Check to see if this is the head node.
+			if(node == *processes) {
+				*processes = node->next;
+				if(*processes != NULL) {
+					(*processes)->prev = NULL;
+				}
+			} else {
+				// We are not at the head
+				node->prev->next = node->next;
+				node->next->prev = node->prev;
+			}
+			free(node);
+			break;
+		}
+		// Not a match keep going
+		node = node->next;
+	}
+	if(!removed) {
+		warn("The process <%d> does not exist in the list. Unable to remove it.\n", process->pid);
+	}
+finished:
+	return removed;
 }
 
 Process* get_process(Process *processes, const char *ipaddress, unsigned int port) {
@@ -42,6 +78,20 @@ Process* get_process(Process *processes, const char *ipaddress, unsigned int por
 		}
 	} else {
 		info("No processes are currently being tracked.\n");
+	}
+	return found;
+}
+
+Process* get_process_by_pid(Process *processes, pid_t pid) {
+	Process *found = NULL;
+	if(processes != NULL) {
+		Process *node = processes;
+		while(node != NULL) {
+			if(node->pid == pid) {
+				found = node;
+				break;
+			}
+		}
 	}
 	return found;
 }
