b4583da5e7ab579ebe8d5a29cfaa9d65d31c114f
Paul Campbell <paul.campbell@stonybrook.edu>
2014-10-29 23:34:19 -0400

    Debug
    Added support for trying to detect if a terminal has color support.

--- include/debug.h
+++ include/debug.h
@@ -1,16 +1,29 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 #include <stdio.h>
+#include <unistd.h>
 
 // Colors
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[1;31m"
-#define KGRN  "\x1B[1;32m"
-#define KYEL  "\x1B[1;33m"
-#define KBLU  "\x1B[1;34m"
-#define KMAG  "\x1B[1;35m"
-#define KCYN  "\x1B[1;36m"
-#define KWHT  "\x1B[1;37m"
+#if defined(COLOR) && COLOR >= 8
+	#define KNRM  "\x1B[0m"
+	#define KRED  "\x1B[1;31m"
+	#define KGRN  "\x1B[1;32m"
+	#define KYEL  "\x1B[1;33m"
+	#define KBLU  "\x1B[1;34m"
+	#define KMAG  "\x1B[1;35m"
+	#define KCYN  "\x1B[1;36m"
+	#define KWHT  "\x1B[1;37m"
+#else
+	/* Color was either not defined or Terminal did not support */
+	#define KNRM
+	#define KRED
+	#define KGRN
+	#define KYEL
+	#define KBLU
+	#define KMAG
+	#define KCYN
+	#define KWHT
+#endif
 
 #ifdef DEBUG
 	#define debug(S, ...) do{fprintf(stdout, KBLU "DEBUG: %s:%s:%d " S KNRM, __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__);} while(0)
