fcc9fdeede136aa0ef911909efcdb495efd05d3d
Paul Campbell <paul.campbell@stonybrook.edu>
2014-11-01 16:57:33 -0400

    Debug
    Made a brown color instead of blue to help diff between different
    printouts.

--- include/debug.h
+++ include/debug.h
@@ -13,6 +13,7 @@
 	#define KMAG  "\x1B[1;35m"
 	#define KCYN  "\x1B[1;36m"
 	#define KWHT  "\x1B[1;37m"
+	#define KBWN  "\x1B[0;33m"
 #else
 	/* Color was either not defined or Terminal did not support */
 	#define KNRM
@@ -23,19 +24,20 @@
 	#define KMAG
 	#define KCYN
 	#define KWHT
+	#define KBWN
 #endif
 
 #ifdef DEBUG
 	#define debug(S, ...)   fprintf(stdout, KBLU "DEBUG: %s:%s:%d " S KNRM, __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__);
 	#define error(S, ...)   fprintf(stderr, KRED "ERROR: %s:%s:%d " S KNRM, __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__);
 	#define warn(S, ...)    fprintf(stderr, KYEL "WARN: %s:%s:%d " S KNRM, __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__);
-	#define info(S, ...)    fprintf(stdout, KCYN "INFO: %s:%s:%d " S KNRM, __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__);
+	#define info(S, ...)    fprintf(stdout, KBWN "INFO: %s:%s:%d " S KNRM, __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__);
 	#define success(S, ...) fprintf(stdout, KGRN "SUCCESS: %s:%s:%d " S KNRM, __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__);
 #else
 	#define debug(S, ...)
 	#define error(S, ...)   fprintf(stderr, KRED "ERROR: " S KNRM, ##__VA_ARGS__);
 	#define warn(S, ...)    fprintf(stderr, KYEL "WARN: " S KNRM, ##__VA_ARGS__);
-	#define info(S, ...)    fprintf(stdout, KCYN "INFO: " S KNRM, ##__VA_ARGS__);
+	#define info(S, ...)    fprintf(stdout, KBWN "INFO: " S KNRM, ##__VA_ARGS__);
 	#define success(S, ...) fprintf(stdout, KGRN "SUCCESS: " S KNRM, ##__VA_ARGS__);
 #endif
 
