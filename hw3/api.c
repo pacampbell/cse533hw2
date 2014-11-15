30a0782cb9e96f1b8b7dfa672e644e8307ab0c94
Paul Campbell <paul.campbell@stonybrook.edu>
2014-11-15 12:33:18 -0500

    API
    Stubbed out the api based on the homework assignment description.

--- hw3/api.c
+++ hw3/api.c
@@ -1,1 +1,9 @@
 #include "api.h"
+
+ssize_t msg_send(int sd, const char *canonicalIP, int port, void *msg, int flag) {
+    return -1;
+}
+
+ssize_t msg_recv(int sd, void *msg, const char *canonicalIP, int *port) {
+    return -1;
+}
--- hw3/api.h
+++ hw3/api.h
@@ -1,4 +1,9 @@
 #ifndef API_H
 #define API_H
+#include <sys/types.h>
+
+ssize_t msg_send(int sd, const char *canonicalIP, int port, void *msg, int flag);
+
+ssize_t msg_recv(int sd, void *msg, const char *canonicalIP, int *port);
 
 #endif
