5ee736c4f4899b30635649e2e32d589b184cf815
Paul Campbell <paul.campbell@stonybrook.edu>
2014-11-17 18:28:49 -0500

    Client
    Started work on the client

--- hw3/client.c
+++ hw3/client.c
@@ -1,1 +1,60 @@
 #include "client.h"
+
+int main(int argc, char *argv[]) {
+    int success = EXIT_SUCCESS;
+    char temp_name[] = "hw3_tempXXXXXX";
+    int sock_fd;
+    int named_pipe;
+    struct sockaddr_un local;
+    // Attempt to open the temp file.
+    if((named_pipe = mkstemp(temp_name)) < 0) {
+        error("Failed to open the tempfile %s\n", temp_name);
+        success = EXIT_FAILURE;
+        goto EXIT;
+    }
+    // Attempt to get a socket
+    if((sock_fd = socket(AF_LOCAL, SOCK_DGRAM, 0)) < 0) {
+        error("Failed to get a socket\n");
+        success = EXIT_FAILURE;
+        goto REMOVE_TMP;
+    }
+    // Attempt to bind to the socket
+    memset(&local, 0, sizeof(struct sockaddr_un));
+    // Copy the path to the temp file to the local.sun_path
+    local.sun_family = AF_LOCAL;
+    strcpy(local.sun_path, temp_name);
+    // Attempt to bind socket
+    if(bind(sock_fd, (struct sockaddr*)&local, sizeof(local)) < 0) {
+        error("Failed to bind to the sock_fd: %d\n", sock_fd);
+        success = EXIT_FAILURE;
+        goto CLOSE_SOCK_FD;
+    }
+    // If we got this far we can attempt to enter the run loop
+    success = run(sock_fd);
+
+CLOSE_SOCK_FD:
+    // Attempt to close the open socket
+    if(sock_fd >= 0) {
+        if(close(sock_fd) > 0) {
+            debug("Successfully closed sock_fd: %d\n", sock_fd);
+        } else {
+            warn("Failed to close sock_fd: %d\n", sock_fd);
+        }
+    }
+REMOVE_TMP:
+    // Attempt to remove the temp file
+    if(named_pipe >= 0) {
+        if(unlink(temp_name) == 0) {
+            debug("Sucessfully removed %s\n", temp_name);
+        } else {
+            warn("Failed to remove %s\n", temp_name);
+        }
+    }
+EXIT:
+    return success;
+}
+
+int run(int sock_fd) {
+    
+    return EXIT_SUCCESS;
+}
--- hw3/client.h
+++ hw3/client.h
@@ -1,4 +1,23 @@
 #ifndef CLIENT_H
 #define CLIENT_H
+// libc headers
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+// System headers
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+// Program headers
+#include "debug.h"
+#include "api.h"
+// Program prototypes
 
+/**
+ * Starts running the client program.
+ * @param sock_fd File descriptor bound to the temp file used for IPC.
+ * @return Returns the success of failure conditions of the running process.
+ */
+int run(int sock_fd);
 #endif
